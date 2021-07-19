fe044c14b8875338156dd27afbe47af516e16111
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-07-19 11:41:22 +0700

    add middleware

--- go-training-restful/main.go
+++ go-training-restful/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"alta/training/config"
+	"alta/training/middlewares"
 	"alta/training/routes"
 	"fmt"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
+	e := echo.New()
 	config.InitDb()
 	config.InitPort()
-	e := routes.New()
+	middlewares.LogMiddlewares(e)
+	routes.New(e)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.HTTP_PORT)))
 }
--- go-training-restful/routes/routes.go
+++ go-training-restful/routes/routes.go
@@ -2,12 +2,20 @@ package routes
 
 import (
 	"alta/training/controllers"
+	"alta/training/middlewares"
+	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-func New() *echo.Echo {
-	e := echo.New()
+func New(e *echo.Echo) {
 	e.GET("/users", controllers.GetUserControllers)
-	return e
+	eAuth := e.Group("")
+	eAuth.Use(middleware.BasicAuth(middlewares.BasicAuthDb))
+	eAuth.GET("/rahasia", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "rahasia, cuma bisa diakses yang login",
+		})
+	})
 }
